f850f127181396f073bc3617def4850642c1b2c3
soonupy\순우 <soonupy97@naver.com>
2026-01-30 00:45:12 +0900

    커밋 테스트

--- src/contents/Intro.tsx
+++ src/contents/Intro.tsx
@@ -4,7 +4,6 @@ function Intro() {
 
   return (
     <section className="hero-container intro-container">
-        <div className='inner'>
           <FallingText
       text={`React 퍼블리셔로서 사용자 경험을 고민하고, \n인터랙티브한 UI와 애니메이션으로 브랜드 가치를 전달합니다.`}
       highlightWords={["React", "퍼블리셔", "사용자 경험", "인터랙티브", "애니메이션", "브랜드"]}
@@ -15,6 +14,7 @@ function Intro() {
       gravity={0.1}
       mouseConstraintStiffness={1}
     />
+        <div className='inner'>
     <h3>
     안녕하세요! 프론트엔드 개발과 UI/UX 디자인하는 soonupy의 포트폴리오입니다.
     <br />
